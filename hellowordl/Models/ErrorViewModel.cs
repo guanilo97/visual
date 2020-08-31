7e644f5973fa3a3800666ce7bfe65a916b01380f
GUANILO <guanilo97>
2020-08-30 23:35:04 -0500

    version1

---
+++ hellowordl/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace hellowordl.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
